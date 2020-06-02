4aef5b3eb003df5f7d81c68deb6253b5fdbf8a11
Parker Williamson <parker.williamson@gmail.com>
2020-06-02 21:08:09 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -15,7 +15,7 @@ def select_name_and_motto_of_char_with_longest_motto
 end
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species, COUNT 
+  "SELECT species, COUNT (species)
    FROM characters 
    
    "
