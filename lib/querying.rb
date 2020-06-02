3e8ae09459629a8db1f95434f9f1c3bcfd07559d
Parker Williamson <parker.williamson@gmail.com>
2020-06-02 21:45:38 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -49,5 +49,9 @@ end
 
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT (character_books.book_id)
+   JOIN character_books 
+   ON characters.id = character_books.character_id 
+   GROUP BY name 
+   ORDER BY name DESC;
   "
 end
