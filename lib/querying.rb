9e1a29f5bd129c9731b49b6f6d310c6b2659a8d7
Parker Williamson <parker.williamson@gmail.com>
2020-06-02 21:44:43 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -48,6 +48,6 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, books.
+  "SELECT characters.name, COUNT (character_books.book_id)
   "
 end
